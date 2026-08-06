184825d012f5a66905e37fed6316617aa301e122
Wonkmy <wonkmy@gmail.com>
2026-08-07 00:37:45 +0800

    1 丰富了变真假玩法的功能，现在可以选择物品了，并且物品有溢价，合适就拿下放入背包，还能看视频直接拿下   2 专家三选一界面需要加一个看视频重roll，功能还没完成  3 辨真假玩法还要打磨

---
+++ assets/scripts/GameCodes/ZhenJiaGoods.ts
@@ -0,0 +1,54 @@
+import { FaynUtils } from "../Global/FaynUtils";
+import ZhenjiaPanel from "../Panels/ZhenjiaPanel";
+import { UIManager } from "../UIManager/UIManager";
+import { ItemInstance } from "./Datas/GameData";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class ZhenJiaGoods extends cc.Component {
+    _material:cc.Material = null!;
+    _sprite:cc.Sprite = null!;
+    private _isSelected: boolean = false;
+
+    _itemInstance:ItemInstance = null!;
+    ID:number  = 0;
+
+    finalPrice:number = 0;
+
+    protected onLoad(): void {
+        this.ID = Number(this.node.name.slice(5,6))
+        this._sprite = this.node.getChildByName("view").getComponent(cc.Sprite);
+        this.node.getChildByName("view").on(cc.Node.EventType.TOUCH_END,this.onTapSelf,this)
+    }
+
+    setData(itemInstance:ItemInstance){
+        this._itemInstance = itemInstance;
+        this.finalPrice = Math.floor(this._itemInstance.buyPrice + this._itemInstance.buyPrice * (Math.random() * this.ID));
+    }
+
+    clearData(){
+        this._itemInstance = null!;
+        this._sprite.spriteFrame = null!;
+        this._isSelected = false;
+        ZhenjiaPanel.instance.hideAllGoodsSelectedIcon();
+        ZhenjiaPanel.instance.showCost(0);
+        UIManager.getInstance().closeUI(ZhenjiaPanel);
+    }
+
+
+    private onTapSelf(){
+        FaynUtils.PlayMusic("bullet",false,1);
+        ZhenjiaPanel.instance.hideAllGoodsSelectedIcon();
+        if(this._isSelected){
+            this._isSelected  = false;
+            ZhenjiaPanel.instance.curSelectedGoodsIndex = -1;
+        }else{
+            this._isSelected  = true;
+            ZhenjiaPanel.instance.curSelectedGoodsIndex = this.ID;
+            this.node.getChildByName("num").active = true;
+
+            ZhenjiaPanel.instance.showCost(this.finalPrice)
+        }
+    }
+}
--- assets/scripts/Panels/ZhenjiaPanel.ts
+++ assets/scripts/Panels/ZhenjiaPanel.ts
@@ -5,10 +5,13 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
 
+import { Advertise } from "../GameCodes/Advertise";
 import { ItemInstance } from "../GameCodes/Datas/GameData";
+import ZhenJiaGoods from "../GameCodes/ZhenJiaGoods";
 import GameMain from "../GameMain";
 import { BaseUI } from "../UIManager/BaseUI";
 import { UIManager } from "../UIManager/UIManager";
+import DialogPanel from "./DialogPanel";
 
 const {ccclass, property} = cc._decorator;
 
@@ -16,45 +19,110 @@ const {ccclass, property} = cc._decorator;
 export default class ZhenjiaPanel extends BaseUI {
     protected static className = "ZhenjiaPanel";
 
+    static instance:ZhenjiaPanel=null!;
+
     @property({type:cc.Node})
     closeBtn:cc.Node = null!;
 
     goodsSpriteFrame: cc.SpriteFrame = null!;
     itemInstance:ItemInstance = null!;
 
+    curSelectedGoodsIndex:number = -1;
+
+    protected onLoad(): void {
+        ZhenjiaPanel.instance = this;
+    }
+
     override onShow(): void {
         this.closeBtn.on(cc.Node.EventType.TOUCH_END, () => {
             UIManager.getInstance().closeUI(ZhenjiaPanel);
         }, this)
+
+        this.hideAllGoodsSelectedIcon();
+        this.upgradeTotalMoney();
+        this.node.getChildByName("btn_buy").on(cc.Node.EventType.TOUCH_END,this.onBuy ,this)
+    }
+
+    onBuy(){
+        let price = this.node.getChildByName("goods" + this.curSelectedGoodsIndex).getComponent(ZhenJiaGoods).finalPrice;
+        let _itemIns = this.node.getChildByName("goods" + this.curSelectedGoodsIndex).getComponent(ZhenJiaGoods)._itemInstance;
+        if(GameMain.instance.mainRuntime.ctx.totalMoney >=  price){
+            GameMain.instance.mainRuntime.ctx.addMoney(-price);
+            this.upgradeTotalMoney();
+            GameMain.instance.mainRuntime.ctx.inventoryItemInstance.push(_itemIns);
+            cc.sys.localStorage.setItem("bag_data",JSON.stringify(GameMain.instance.mainRuntime.ctx.inventoryItemInstance));
+            UIManager.getInstance().openUI(DialogPanel, 2, (ui: DialogPanel) => {
+                ui.onShow();
+                ui.setContent(`太棒了，你入手了${_itemIns.name}`, () => {
+                    UIManager.getInstance().closeUI(DialogPanel);
+                    this.node.getChildByName("goods" + this.curSelectedGoodsIndex).getComponent(ZhenJiaGoods).clearData();
+                }, false)
+            })
+        }else{
+            UIManager.getInstance().openUI(DialogPanel, 2, (ui: DialogPanel) => {
+                ui.onShow();
+                ui.setContent(`预算不够了，是否观看视频后直接拿下？`, () => {
+                    UIManager.getInstance().closeUI(DialogPanel);
+                    Advertise.instance.ShowVideoAd((res:number)=>{
+                        if (res === 1) {
+                            GameMain.instance.mainRuntime.ctx.inventoryItemInstance.push(_itemIns);
+                            cc.sys.localStorage.setItem("bag_data", JSON.stringify(GameMain.instance.mainRuntime.ctx.inventoryItemInstance));
+                            UIManager.getInstance().openUI(DialogPanel, 2, (ui: DialogPanel) => {
+                                ui.onShow();
+                                ui.setContent(`太棒了，你入手了${_itemIns.name}`, () => {
+                                    UIManager.getInstance().closeUI(DialogPanel);
+                                    this.node.getChildByName("goods" + this.curSelectedGoodsIndex).getComponent(ZhenJiaGoods).clearData();
+                                }, false)
+                            })
+                        }
+                    })
+                }, true)
+            })
+        }
+    }
+
+    hideAllGoodsSelectedIcon() {
+        for (let i = 0; i < 6; i++) {
+            this.node.getChildByName("goods" + (i + 1)).getChildByName("num").active = false;
+        }
+    }
+
+    showCost(num:number){
+        this.node.getChildByName("btn_buy").getChildByName("cost").getComponent(cc.Label).string = String(num);
     }
 
     setGoods(itenInstance: ItemInstance) {
-                if (!itenInstance) {
-                    console.warn("ZhenjiaPanel.setResultSprite: itenInstance is null");
-                    return;
-                }
-                this.itemInstance = itenInstance;
+        if (!itenInstance) {
+            console.warn("ZhenjiaPanel.setResultSprite: itenInstance is null");
+            return;
+        }
+        this.itemInstance = itenInstance;
 
-                GameMain.instance.bundle.load("arts/items/"+itenInstance.image, cc.SpriteFrame, (err, spriteFrame: cc.SpriteFrame) => {
-                    if (err) {
-                        console.error("ZhenjiaPanel.setResultSprite: load spriteFrame error:", err);
-                        return;
-                    }
-
-                    this.goodsSpriteFrame = spriteFrame;
-                    const source = this.goodsSpriteFrame;
-                    const rect = source.getRect();
-
-
-                    for (let i = 0; i < 6; i++) {
-                        var _color = this.getColor();
-                        this.node.getChildByName("goods"+(i + 1)).getComponent(cc.Sprite).spriteFrame = this.goodsSpriteFrame;
-                        this.node.getChildByName("goods"+(i + 1)).color = _color;
-                        this.node.getChildByName("goods"+(i + 1)).width = rect.width;
-                        this.node.getChildByName("goods"+(i + 1)).height = rect.height;
-                    }
-                });
+        GameMain.instance.bundle.load("arts/items/" + itenInstance.image, cc.SpriteFrame, (err, spriteFrame: cc.SpriteFrame) => {
+            if (err) {
+                console.error("ZhenjiaPanel.setResultSprite: load spriteFrame error:", err);
+                return;
             }
+
+            this.goodsSpriteFrame = spriteFrame;
+            const source = this.goodsSpriteFrame;
+            const rect = source.getRect();
+
+
+            for (let i = 0; i < 6; i++) {
+                var _color = this.getColor();
+                this.node.getChildByName("goods" + (i + 1)).getChildByName("view").getComponent(cc.Sprite).spriteFrame = this.goodsSpriteFrame;
+                this.node.getChildByName("goods" + (i + 1)).getChildByName("view").color = _color;
+                this.node.getChildByName("goods" + (i + 1)).getChildByName("view").width = rect.width;
+                this.node.getChildByName("goods" + (i + 1)).getChildByName("view").height = rect.height;
+                this.node.getChildByName("goods" + (i + 1)).getComponent(ZhenJiaGoods).setData(itenInstance);
+            }
+        });
+    }
+
+    upgradeTotalMoney(){
+        this.node.getChildByName("totalMoney").getChildByName("content").getComponent(cc.Label).string = "总预算: "+ String(GameMain.instance.mainRuntime.ctx.totalMoney);
+    }
 
 
     getColor(){
